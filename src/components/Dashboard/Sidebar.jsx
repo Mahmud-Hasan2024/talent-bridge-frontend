0fb695dad9cd584d7bc60e46c99621ee46f91ed9
Mahmud Hasan <mh.kid234@gmail.com>
2026-04-28 12:03:27 +0600

    Return to home button added in sidebar.jsx

--- src/components/Dashboard/Sidebar.jsx
+++ src/components/Dashboard/Sidebar.jsx
@@ -118,7 +118,7 @@ const Sidebar = () => {
         </div>
 
         {/* Footer */}
-        <div className="mt-6 pt-4 text-xs text-base-content/70 border-t border-base-300">
+        <div className="pt-4 text-[10px] uppercase tracking-widest text-base-content/70 text-center">
           © 2025 Talent Bridge Co.
         </div>
       </aside>
